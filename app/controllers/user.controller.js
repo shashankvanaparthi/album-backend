6ed78f7adb34ddcf972393c2225e6893e5d9e716
shashankvanaparthi <91704462+shashankvanaparthi@users.noreply.github.com>
2022-05-24 12:51:48 -0500

    User Authentication completed

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -6,12 +6,17 @@ const User = db.users;
 const Op = db.Sequelize.Op;
 
 exports.signup = async (req, res) => {
+  const user = await User.findOne({ where : {email : req.body.email }});
+  if(user){
+    res.status(409).json({message:"User Already Exist"})
+  }
   const salt = await bcrypt.genSalt(10);
   var usr = {
     name : req.body.name,
     email : req.body.email,
     password : await bcrypt.hash(req.body.password, salt)
   };
+
   created_user = await User.create(usr);
   console.log(created_user)
   res.status(201).json(created_user);
@@ -23,10 +28,10 @@ exports.login = async (req,res) =>{
        const password_valid = await bcrypt.compare(req.body.password,user.password);
        if(password_valid){
            token = jwt.sign({ "id" : user.id,"email" : user.email,"name":user.name },process.env.SECRET);
-           res.status(200).json({ token : token });
+           res.status(200).json({ token : token, userId: user.id, userName:user.name });
        } else {
          res.status(400).json({ error : "Password Incorrect" });
-       }    
+       }
      }else{
        res.status(404).json({ error : "User does not exist" });
      }
