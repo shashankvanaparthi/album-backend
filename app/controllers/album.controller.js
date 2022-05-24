6f7d51161e9e4730299bdb9f0ba366cd26e78344
Biswitha <biswithareddy@gmail.com>
2022-05-24 11:40:50 -0500

    Tracks and Albums Crud Done

--- app/controllers/album.controller.js
+++ app/controllers/album.controller.js
@@ -1,29 +1,55 @@
+const { response } = require('express');
 const db = require('../models')
 
 const Album = db.album;
 const Op = db.Sequelize.Op;
+const User = db.users;
 
 //Methods to perform crud operations
-exports.saveAlbum = (req,res)=>{
+exports.saveAlbum = async (req, res) => {
     console.log("Save Album is called")
+    const userId = req.body.userId;
+    const album = req.body.album;
+    const user = await User.findOne({ where: { id: userId } })
+    if (user) {
+        console.log(user)
+        user.createAlbum(album).then(response => {
+            res.status(200).json(response)
+        }, error => {
+            res.status(400).json({"message":"Some Error occured"})
+            console.log(error)
+        })
+    }
 }
 
-exports.getAllAlbums = (req,res)=>{
-    console.log("getAll Albums is called")
+exports.getAllAlbums =async (req, res) => {
+    const userId = req.query.userId;
+    const albums = await Album.findAll({where:{userId:userId}})
+    res.status(200).json(albums);
 }
 
-exports.getAlbumById = (req,res)=>{
+exports.getAlbumById = async (req, res) => {
     console.log("get Album by Id is called")
+    const userId = req.query.userId;
+    const albumId = req.query.albumId;
+    const albums = await  Album.findAll({where:{userId:userId,id:albumId}})
+    res.status(200).json(albums)
 }
 
-exports.deleteAlbumById = (req,res)=>{
-    console.log("Delete Albumby Id is called")
+exports.deleteAlbumById =async (req, res) => {
+    const albumId = req.params.id
+    const status = await Album.destroy({where:{id:albumId}})
+    if(status){
+      res.status(200).json({"message":"Delete Success"})
+    }
+    res.status(400).json({"message":"Delete Failed"})
 }
 
-exports.deleteAllAlbum = (req,res)=>{
+exports.deleteAllAlbum = (req, res) => {
     console.log("Delete All Albums is called")
 }
 
-exports.updateAlbum = (req,res)=>{
+
+exports.updateAlbum = (req, res) => {
     console.log("update Album is called")
 }
--- app/controllers/track.controller.js
+++ app/controllers/track.controller.js
@@ -2,26 +2,52 @@ const db = require('../models')
 
 const Track = db.track;
 const Op = db.Sequelize.Op;
+const Album = db.album
 
 //Methods to perform crud operations
-exports.saveTrack = (req,res)=>{
+exports.saveTrack = async (req,res)=>{
     console.log("Save Track is called")
+    const albumId = req.body.albumId;
+    const track = req.body.track;
+    const album = await Album.findOne({ where: { id: albumId } })
+    if (album) {
+        console.log(album)
+        album.createTrack(track).then(response => {
+            res.status(200).json(response)
+        }, error => {
+            res.status(400).json({"message":"Some Error occured"})
+            console.log(error)
+        })
+    }
 }
 
-exports.getAllTracks = (req,res)=>{
+exports.getAllTracks =async (req,res)=>{
     console.log("getAll Tracks is called")
+    const albumId = req.query.albumId;
+    const tracks = await Track.findAll({where:{albumId:albumId}})
+    res.status(200).json(tracks);
 }
 
-exports.getTracksById = (req,res)=>{
-    console.log("get Track by Id is called")
+exports.getTracksById =async (req,res)=>{
+  console.log("get Track by Id is called")
+  const albumId = req.query.albumId;
+  const trackId = req.query.trackId;
+  const tracks = await  Track.findAll({where:{albumId:albumId,id:trackId}})
+  res.status(200).json(tracks)
 }
 
 exports.deleteAllTracks = (req,res)=>{
     console.log("Delete All Tracks is called")
 }
 
-exports.deleteTrackById = (req,res)=>{
+exports.deleteTrackById = async (req,res)=>{
     console.log("Delete Track By Id is called")
+    const trackId = req.params.id
+    const status = await Track.destroy({where:{id:trackId}})
+    if(status){
+      res.status(200).json({"message":"Delete Success"})
+    }
+    res.status(400).json({"message":"Delete Failed"})
 }
 
 exports.updateTrack = (req,res)=>{
